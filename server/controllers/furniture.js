9712294b866b9061ba9530e2109a71b6ffc50581
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-01-31 21:01:35 +0200

    Add database validation and error handling

--- server/controllers/furniture.js
+++ server/controllers/furniture.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { isAuth } = require('../middlewares/guards');
 const { getAll, getById, create } = require('../services/furniture');
+const { parseError } = require('../util');
 
 
 router.get('/', async (req, res) => {
@@ -12,14 +13,21 @@ router.post('/', isAuth(), async (req, res) => {
     const data = {
         make: req.body.make,
         model: req.body.model,
-        year: req.body.year,
+        year: Number(req.body.year),
         description: req.body.description,
-        price: req.body.price,
+        price: Number(req.body.price),
         img: req.body.img,
         material: req.body.material,
         owner: req.user._id
     };
-    const result = await create(data);
+    try {
+        const result = await create(data);
+        res.status(201).json(result);
+
+    } catch (err) {
+        const message = parseError(err);
+        res.status(err.status || 400).json({ message });
+    }
 
     res.status(201).json(result.body);
 });
--- server/models/Furniture.js
+++ server/models/Furniture.js
@@ -1,14 +1,48 @@
 const { Schema, model } = require('mongoose');
 
 const furnitureSchema = new Schema({
-    make: { type: String },
-    model: { type: String },
-    year: { type: Number },
-    description: { type: String },
-    price: { type: Number },
-    img: { type: String },
-    material: { type: String },
-    owner: { type: Schema.Types.ObjectId, ref: 'User' }
+    make: {
+        type: String,
+        required: [true, 'Make is required'],
+        minLength: [4, 'Make must be at least 4 characters length']
+    },
+
+    model: {
+        type: String,
+        required: [true, 'Model is required'],
+        minLength: [4, 'Make must be at least 4 characters length']
+    },
+
+    year: {
+        type: Number,
+        required: [true, 'Model is required!'],
+        min: [1950, 'Year must be between 1950 and 2050'],
+        max: [2050, 'Year must be between 1950 and 2050'],
+    },
+
+    description: {
+        type: String,
+        required: [true, 'Model is required'],
+        minLength: [10, 'Description must be at least 10 characters length'] },
+
+    price: {
+        type: Number,
+        required: [true, 'Model is required'],
+        min: [0.01, 'Price must be greater than 0.1']
+    },
+
+    img: {
+        type: String,
+        required: [true, 'Model is required']
+    },
+
+    material: {
+        type: String
+    },
+
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User' }
 });
 
 module.exports = model('Furniture', furnitureSchema);
