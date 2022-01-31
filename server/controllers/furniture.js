e637c2c1c0916db8364738fb4ddcf165c956bd60
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-01-31 16:23:44 +0200

    Implement create and get details functions

--- server/controllers/furniture.js
+++ server/controllers/furniture.js
@@ -1,9 +1,31 @@
 const router = require('express').Router();
-const { getAll } = require('../services/furniture');
+const { getAll, getById, create } = require('../services/furniture');
+
 
 router.get('/', async (req, res) => {
     const data = await getAll();
     res.json(data);
+});
+
+router.post('/', async (req, res) => {
+    const data = {
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        description: req.body.description,
+        price: req.body.price,
+        img: req.body.img,
+        material: req.body.material
+    };
+    const result = await create(data);
+
+    res.status(201).json(result.body);
+});
+
+router.get('/:id', async (req, res) => {
+    const item = await getById(req.params.id);
+    console.log(item);
+    res.json(item);
 });
 
 
--- server/services/furniture.js
+++ server/services/furniture.js
@@ -5,11 +5,19 @@ async function getAll() {
     return Furniture.find({}).lean();
 }
 
+async function create(data) {
+    const res = new Furniture(data);
+    await res.save();
+
+    return res;
+}
+
 async function getById(id) {
     return Furniture.findById(id).lean();
 }
 
 module.exports = {
     getAll,
-    getById
+    getById,
+    create
 };
